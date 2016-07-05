644a74f0598ef0802f492d38176c1e60d81a3b36
ManigandaPerumal <smanigandan08@gmail.com>
2016-07-05 17:26:29 +0530

    First Commit Author Change

---
+++ src/CoreJava/DataTypes.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class DataTypes {
+
+	int iteration_count = 0;
+	String test_string = "Tag4QA testing training academy";
+	char test_char = 'T';
+	float test_float = 2.56f;
+	long test_long = 2365648564l;
+	double test_double = 2.65846654d;	
+}
