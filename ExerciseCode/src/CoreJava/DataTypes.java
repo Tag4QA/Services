63859e30cfcf5cf358896070c74ff55b9d81fc9d
prasath246 <prasath246@gmail.com>
2016-07-13 22:18:14 +0530

    Update DataTypes.java
    Update company name in string datatype.

--- ExerciseCode/src/CoreJava/DataTypes.java
+++ ExerciseCode/src/CoreJava/DataTypes.java
@@ -3,7 +3,7 @@ package CoreJava;
 public class DataTypes {
 
 	int iteration_count = 0;
-	String test_string = "Tag4QA testing training academy";
+	String test_string = "Tag4QA Test Automation Group for QA";
 	char test_char = 'T';
 	float test_float = 2.56f;
 	long test_long = 2365648564l;
